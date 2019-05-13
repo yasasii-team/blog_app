f3715ed13ea9483b130babe1baa3be7e501121d0
Izumi Toshinori <toshinori622@gmail.com>
2019-05-13 20:28:07 +0900

    postsのデータを全て取得するメソッドと取得するデータ構造をオブジェクトのListにする処理を追加

--- blog_app/DBManager.py
+++ blog_app/DBManager.py
@@ -5,12 +5,18 @@ import sqlite3
 class DBManager():
     def __init__(self):
         self.db = sqlite3.connect(app.config["DATABASE"])
+        self.db.row_factory = sqlite3.Row
         self.cursor = self.db.cursor()
 
     def init_db(self):
         with app.open_resource("schema.sql") as f:
             self.cursor.executescript(f.read().decode("utf-8"))
         self.db.commit()
+
+    def get_all_posts(self):
+        sql = "select * from posts order by created_at desc"
+        self.cursor.execute(sql)
+        return self.cursor.fetchall()
 
     def close(self):
         self.cursor.close()
