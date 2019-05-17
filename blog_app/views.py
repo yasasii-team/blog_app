67a7f72cb6940d76f084eff2c295360463391c2f
渡邉龍之介 <bowplusplus@gmail.com>
2019-05-18 03:05:20 +0900

    [WIP]index.htmlにpostsデータ全てをリスト表示できるように修正。post_detail.htmlを追加。views.pyを、indexのルーティングをDBManager.get_all_postsから全postデータを取得するよう修正、post_detailをfindからidに紐づくデータを取得するよう追加。

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,7 +1,22 @@
 # coding: utf-8
+from flask import Flask, render_template
 from blog_app import app
+from DBManager import *
 
-
+#下記動作未確認です。5/19
 @app.route('/')
 def index():
-    return "Hello World"
+    posts = DBManager.get_all_posts()
+    return render_template('index.html',posts = posts)
+
+#下記動作未確認です。5/19
+@app.route('post_detail/<post_id>')
+def post_detail(post_id):
+    title = DBManager.find(post_id).title
+    body = DBManager.find(post_id).body
+    #titleとbodyを引き出したいと思っていますが、引き出し方調査不足で未実装です。
+    return render_template('post_detail.html',title = title, body = body)
+
+# @app.route('/')
+# def index():
+#     return "Hello World"
