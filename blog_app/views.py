89c1f03840a314c1adc0401baeb65819544c47ee
nyakatama <nyaka3@gmail.com>
2019-05-20 01:37:00 +0900

    記事新規作成機能追加
    現在はDBへのアクセス部分は直接SQLを書いています。

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 import os
 from blog_app import app
-import datetime
+from datetime import datetime
 import sqlite3
 from flask import Flask, render_template, request, url_for
 from flask_wtf.csrf import CSRFProtect
@@ -21,12 +21,13 @@ def index():
 @app.route('/add', methods=['GET', 'POST'])
 def create_page():
     #ユーザー登録処理実装後に要修正(ログインチェック)
-    user_id = 'test_user'
+    user_id = 1
 
     #POST:登録処理
     if request.method == 'POST':
         title = request.form['title']
         body = request.form['body']
+        date_time = datetime.now()
 
         if not title or not body:
             alert = 'タイトルと本文は必須入力です'
@@ -36,18 +37,18 @@ def create_page():
             c = con.cursor()
 
             #DBへのinsert関数ができたらそちらを使う
-            #sqlite3.OperationalError: table posts has 4 columns but 3 values were supplied
-            #idはautoincrement？
-            c.execute('INSERT INTO posts VALUES (?,?,?)',(user_id,title,body))
+            c.execute('INSERT INTO posts(user_id, title, body, created_at, updated_at) \
+                values(?, ?, ?, ?, ?)',\
+                (user_id, title, body, date_time, date_time))
             con.commit()
 
             #DBからのselect関数ができたらそちらを使う
-            result = con.execute('''select * from posts order by id desc''')
+            result = con.execute('''select * from posts order by updated_at desc''')
 
             #成功したら詳細画面へ？（とりあえず、一覧が載ってるindexへ）
             return render_template('index.html', result=result)
 
     #登録画面へ
-    if request.method == 'GET':
+    else:
         return render_template('add.html')
 
