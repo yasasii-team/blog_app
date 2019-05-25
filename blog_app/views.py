5f12e3e0836faa26bd4080bbc32ace7088a64bb4
Izumi Toshinori <toshinori622@gmail.com>
2019-05-25 16:23:45 +0900

    投稿削除機能の実装

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 from blog_app import app
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, abort,request
 from blog_app.DBManager import DBManager
 
 
@@ -14,6 +14,15 @@ def post_detail(post_id):
     post = DBManager().get_post(post_id)
     return render_template('post_detail.html',post = post)
 
+@app.route('/delete', methods=['POST'])
+def delete():
+    id = request.json['id']
+    db_manager = DBManager()
+    if db_manager.delete_post(id):
+        result = {'id': id}
+        return jsonify(result), 201
+    else:
+        return abort(403)
 
 # @app.route('/')
 # def index():
