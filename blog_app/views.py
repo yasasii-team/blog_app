d407a5655f805398378b26ec91ddc759c881dfd8
nyakatama <nyaka3@gmail.com>
2019-05-25 17:34:13 +0900

    記事登録/修正機能　修正
    指摘していただいた個所の修正

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,17 +1,9 @@
 # coding: utf-8
 import os
 from blog_app import app
-from datetime import datetime
-import sqlite3
 from blog_app.DBManager import DBManager
 from flask import Flask, render_template, request, url_for, redirect
 from flask_wtf.csrf import CSRFProtect
-#後で実装する用
-#from flask_wtf import FlaskForm
-#from wtforms import StringField, SubmitField
-#from wtforms.validators import Required
-from flask import Flask, render_template
-from blog_app.DBManager import DBManager
 
 app.config['SECRET_KEY'] = os.urandom(24)
 csrf = CSRFProtect(app)
@@ -28,52 +20,44 @@ def post_detail(post_id):
 
 @app.route('/add', methods=['GET', 'POST'])
 def create_page():
-    try:
-        data= {'title': '', 'body': ''}
+    data= {'title': '', 'body': ''}
 
-        #ユーザー登録処理実装後に要修正(ログインチェック)
-        #セッションで管理するから辞書に入れる必要はないかも
-        data['user_id'] = 1
+    #ユーザー登録処理実装後に要修正(ログインチェック)
+    data['user_id'] = 1
 
-        #POST:登録処理
-        if request.method == 'POST':
-            data['title'] = request.form['title']
-            data['body'] = request.form['body']
+    #POST:登録処理
+    if request.method == 'POST':
+        data['title'] = request.form['title']
+        data['body'] = request.form['body']
 
-            if not data['title'] or not data['body']:
-                alert = 'タイトルと本文は必須入力です'
-                return render_template('add.html', alert=alert, data=data)
-            else:
-                blogDB = DBManager()
-                result = blogDB.create_post(data['user_id'], data['title'], data['body'])
-                blogDB.close()
-
-                if result:
-                    # 成功したら詳細画面へ？（とりあえず、一覧が載ってるindexへ）
-                    # post_id不明
-                    # return redirect(url_for('post_detail'), post_id=post_id)
-                    return redirect(url_for('index'))           
-                else:
-                    alert = 'データベース登録に失敗しました'
-                    return render_template('add.html', alert=alert, data=data)
-
-        #登録画面へ
+        if not data['title'] or not data['body']:
+            alert = 'タイトルと本文は必須入力です'
+            return render_template('add.html', alert=alert, data=data)
         else:
-            return render_template('add.html', data=data)
-            
-    except KeyError as e:
-        print(e)
-        return redirect(url_for('index'))
+            blog_db = DBManager()
+            result = blog_db.create_post(data['user_id'], data['title'], data['body'])
+            blog_db.close()
+
+            if result:
+                return redirect(url_for('index'))           
+            else:
+                alert = 'データベース登録に失敗しました'
+                return render_template('add.html', alert=alert, data=data)
+
+    #登録画面へ
+    else:
+        return render_template('add.html', data=data)
+
 
 def check_and_get_post(id):
     # 該当する投稿があるかのチェック
     if not id:
         alert = "不正なアクセスです" 
-        # render_templateでalert送る？
+        # alertはセッションで送るように要修正
         return redirect(url_for('index'))
 
-    blogDB = DBManager()
-    post = blogDB.get_post(id)
+    blog_DB = DBManager()
+    post = blog_DB.get_post(id)
 
     if len(post) < 1:
         alert = "該当する投稿がありません" 
@@ -81,53 +65,44 @@ def check_and_get_post(id):
 
     return post
 
-
 @app.route('/update/<int:post_id>', methods=['GET', 'POST'])
 def update_page(post_id):
-    try:
-        data= {'title': '', 'body': ''}
+    data= {'title': '', 'body': ''}
 
-        #ユーザー登録処理実装後に要修正(ログインチェック)
-        #セッションで管理するから辞書に入れる必要はないかも
-        data['user_id'] = 1
+    #ユーザー登録処理実装後に要修正(ログインチェック)
+    data['user_id'] = 1
 
-        #POST:更新処理
-        if request.method == 'POST':
+    #POST:更新処理
+    if request.method == 'POST':
 
-            post = check_and_get_post(request.form['id'])
+        post = check_and_get_post(request.form['id'])
 
-            data['id'] = request.form['id']
-            data['title'] = request.form['title']
-            data['body'] = request.form['body']
+        data['id'] = request.form['id']
+        data['title'] = request.form['title']
+        data['body'] = request.form['body']
 
-            if not data['title'] or not data['body']:
-                alert = 'タイトルと本文は必須入力です'
+        if not data['title'] or not data['body']:
+            alert = 'タイトルと本文は必須入力です'
+            return render_template('update.html', alert=alert, data=data, post_id=post_id)
+
+        else:
+            blog_db = DBManager()
+            result = blog_db.update_post(data['id'] ,data['title'], data['body'])
+            blog_db.close()
+
+            if result:
+                return redirect(url_for('post_detail', post_id=data['id']))   
+            else:
+                alert = 'データベース登録に失敗しました'
                 return render_template('update.html', alert=alert, data=data, post_id=post_id)
 
-            else:
-                blogDB = DBManager()
-                result = blogDB.update_post(data['id'] ,data['title'], data['body'])
-                blogDB.close()
-
-                if result:
-                    # 成功したら詳細画面へ？（とりあえず、一覧が載ってるindexへ）
-                    # return redirect(url_for('post_detail'), post_id=data['id'])
-                    return redirect(url_for('index'))           
-                else:
-                    alert = 'データベース登録に失敗しました'
-                    return render_template('update.html', alert=alert, data=data, post_id=post_id)
-
-        #GET：入力
-        else:
-            post = check_and_get_post(post_id)
-            data['id'] = post_id
-            data['title'] = post['title']
-            data['body'] = post['body']
-            return render_template('update.html', data=data, post_id=post_id)
-
-    except KeyError as e:
-        print(e)
-        return redirect(url_for('index'))
+    #GET：入力
+    else:
+        post = check_and_get_post(post_id)
+        data['id'] = post_id
+        data['title'] = post['title']
+        data['body'] = post['body']
+        return render_template('update.html', data=data, post_id=post_id)
 
 # @app.route('/')
 # def index():
